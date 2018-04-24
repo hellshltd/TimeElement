65f6865aef2cfcfb851953765a09dee8267c1f5b
timmyRS <tim@timmyrs.de>
2018-04-24 22:13:47 +0200

    Remove jQuery as Dependency

--- TimeElement.js
+++ TimeElement.js
@@ -2,12 +2,6 @@
 
 function calculateTimeElements(options)
 {
-	let $=window.$||window.jQuery;
-	if(typeof $=="undefined")
-	{
-		console.error("TimeElement.js requires jQuery.");
-		return false
-	}
 	if(typeof options=="undefined")options={};
 	if(typeof options.year=="undefined")options.year="{y}-{m}-{d}";
 	if(typeof options.date=="undefined")options.date="{m}-{d}";
@@ -17,38 +11,37 @@ function calculateTimeElements(options)
 	{
 		if(num<10)num="0"+num;
 		return num
-	};
-	$("[data-time]:not([data-ignore-time-element])").each(function()
+	},elms=document.querySelectorAll("[data-time]:not([data-ignore-time-element])");
+	for(let i=0;i<elms.length;i++)
 	{
-		var now=new Date(),
-		time=($(this).attr("data-time")*1000),
-		date=new Date(time),
+		let elm = elms[i],
+		now=new Date(),
+		time=(elm.getAttribute("data-time")*1000),
 		text="",
-		notime=$(this).is("[data-notime]"),
-		datetime=!notime,
-		date=new Date(date.getTime()+(now.getTimezoneOffset()*-1/60*3600));
-		if(!notime&&(now.getFullYear()==date.getFullYear()&&now.getDate()==date.getDate()&&now.getMonth()==date.getMonth()))
-                {
-                        text=options.time;
-                        datetime=!1
-                }
-                else
-                {
-                        if(datetime)text=options.datetime.split("{date}").join(options.date);
-                        else text=options.date
-                }
-                if(now.getFullYear()!=date.getFullYear())
-                {
-                        if(datetime)text=options.datetime.split("{date}").join(options.year);
-                        else text=options.year
-                }
-                if(datetime)text=text.split("{time}").join(options.time);
-		text=text.split("{h}").join(parseNum(date.getHours()))
+		datetime=!elm.hasAttribute("data-notime"),
+		date=new Date(time+(now.getTimezoneOffset()*-1/60*3600));
+		if(datetime&&(now.getFullYear()==date.getFullYear()&&now.getDate()==date.getDate()&&now.getMonth()==date.getMonth()))
+		{
+			text=options.time;
+			datetime=!1
+		}
+		else
+		{
+			if(datetime)text=options.datetime.split("{date}").join(options.date);
+			else text=options.date
+		}
+		if(now.getFullYear()!=date.getFullYear())
+		{
+			if(datetime)text=options.datetime.split("{date}").join(options.year);
+			else text=options.year
+		}
+		if(datetime)
+			text=text.split("{time}").join(options.time);
+		elm.textContent=text.split("{h}").join(parseNum(date.getHours()))
 			.split("{i}").join(parseNum(date.getMinutes()))
 			.split("{d}").join(parseNum(date.getDate()))
 			.split("{m}").join(parseNum(date.getMonth()+1))
 			.split("{y}").join(parseNum(date.getFullYear()));
-		$(this).html(text)
-	});
+	}
 	return true
 }
